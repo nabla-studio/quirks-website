94006e273cad8bcba725402396fa33b47eccc1de
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-14 21:07:14 +0100

    refactor: :recycle: move showcaseIcons to global consts

--- src/components/bento/MultiChain.tsx
+++ src/components/bento/MultiChain.tsx
@@ -1,69 +1,13 @@
+import { SHOWCASE_ICONS } from "@/utils/globals";
 import Image from "next/image";
 import React from "react";
-
-const showcaseIcons = [
-  [
-    {
-      uri: "/showcase/celestia.png",
-      name: "celestia",
-    },
-    {
-      uri: "/showcase/osmosis.png",
-      name: "osmosis",
-    },
-    {
-      uri: "/showcase/mars.png",
-      name: "mars",
-    },
-    {
-      uri: "/showcase/atom.png",
-      name: "atom",
-    },
-  ],
-  [
-    {
-      uri: "/showcase/desmos.png",
-      name: "desmos",
-    },
-    {
-      uri: "/showcase/evmos.png",
-      name: "evmos",
-    },
-    {
-      uri: "/showcase/dydx.png",
-      name: "dydx",
-    },
-    {
-      uri: "/showcase/cre.png",
-      name: "cre",
-    },
-  ],
-  [
-    {
-      uri: "/showcase/akt.png",
-      name: "akt",
-    },
-    {
-      uri: "/showcase/kuji.png",
-      name: "kuji",
-    },
-    {
-      uri: "/showcase/neutron.png",
-      name: "neutron",
-    },
-    {
-      uri: "/showcase/juno.png",
-      name: "juno",
-    },
-  ],
-];
 
 function MultiChain() {
   return (
     <div className="relative min-h-[334px] overflow-hidden rounded-std border-2 border-zinc-300 border-opacity-30 pl-8 pt-8 xl:col-span-2 xl:col-start-1">
       <p className="text-lg">Multi Chain</p>
       <div className="absolute -bottom-8 left-0 right-0 flex flex-col gap-2 xl:-bottom-9">
-        {showcaseIcons.map((row, i) => (
+        {SHOWCASE_ICONS.map((row, i) => (
           <div key={`row ${i}`} className="flex items-center gap-5">
             {row.map(({ name, uri }) => (
               <Image
---
+++ src/utils/globals.ts
@@ -0,0 +1,56 @@
+export const SHOWCASE_ICONS = [
+  [
+    {
+      uri: "/showcase/celestia.png",
+      name: "celestia",
+    },
+    {
+      uri: "/showcase/osmosis.png",
+      name: "osmosis",
+    },
+    {
+      uri: "/showcase/mars.png",
+      name: "mars",
+    },
+    {
+      uri: "/showcase/atom.png",
+      name: "atom",
+    },
+  ],
+  [
+    {
+      uri: "/showcase/desmos.png",
+      name: "desmos",
+    },
+    {
+      uri: "/showcase/evmos.png",
+      name: "evmos",
+    },
+    {
+      uri: "/showcase/dydx.png",
+      name: "dydx",
+    },
+    {
+      uri: "/showcase/cre.png",
+      name: "cre",
+    },
+  ],
+  [
+    {
+      uri: "/showcase/akt.png",
+      name: "akt",
+    },
+    {
+      uri: "/showcase/kuji.png",
+      name: "kuji",
+    },
+    {
+      uri: "/showcase/neutron.png",
+      name: "neutron",
+    },
+    {
+      uri: "/showcase/juno.png",
+      name: "juno",
+    },
+  ],
+];
