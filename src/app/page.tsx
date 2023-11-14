fb9d88e20ab605498e21880d7064f1cd3a556354
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-14 21:03:46 +0100

    feat: :sparkles: add bento showcase with MultiChain section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import ConnectHero from "@/app/components/ConnectHero";
 import Features from "@/app/components/Features";
+import BentoShowcase from "@/app/components/bento/BentoShowcase";
 
 export default function Home() {
   return (
     <main className="flex flex-col text-white max-xl:px-5">
       <ConnectHero />
       <Features />
+      <BentoShowcase />
     </main>
   );
 }
