f35527006080eea248ccb2a7016812325844d791
Davide Segullo <davidesegullo@gmail.com>
2023-11-14 10:56:58 +0100

    feat: :sparkles: add security policies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,38 @@
 const { resolve } = require("path");
 
+const securityHeaders = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "browsing-topics=(self)",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [...securityHeaders],
+      },
+    ];
+  },
   experimental: {
     ppr: true,
   },
---
+++ src/middleware.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const isDev = process.env.NODE_ENV !== "production";
+
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' ${
+      isDev
+        ? `'unsafe-eval' 'unsafe-inline' 'nonce-${nonce}'`
+        : `'nonce-${nonce}' 'strict-dynamic'`
+    } https://www.googletagmanager.com;
+    style-src 'self' ${isDev ? "'unsafe-inline'" : `'nonce-${nonce}'`};
+    img-src 'self' blob: data: https://quirks.nabla.studio;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    ${
+      isDev
+        ? ""
+        : `
+    block-all-mixed-content;
+    upgrade-insecure-requests;`
+    };
+`;
+
+  const requestHeaders = new Headers();
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set(
+    "Content-Security-Policy",
+    // Replace newline characters and spaces
+    cspHeader.replace(/\s{2,}/g, " ").trim()
+  );
+
+  return NextResponse.next({
+    headers: requestHeaders,
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
