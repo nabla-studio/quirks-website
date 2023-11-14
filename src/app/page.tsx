735ec0506de1959004825eda280ef1e844bf6cbd
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-14 21:03:28 +0100

    feat: :lipstick: make px-5 mobile-only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Features from "@/app/components/Features";
 
 export default function Home() {
   return (
-    <main className="flex flex-col px-5 text-white">
+    <main className="flex flex-col text-white max-xl:px-5">
       <ConnectHero />
       <Features />
     </main>
