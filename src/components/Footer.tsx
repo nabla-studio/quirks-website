8176d63e44b2637dde05af7a45bac98463574777
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-16 12:01:21 +0100

    refactor: :children_crossing: update Footer semantics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="mb-4 mt-32 flex flex-col px-5 text-white xl:mb-16 xl:mt-40">
+    <footer className="mb-4 mt-32 flex flex-col px-5 text-white xl:mb-16 xl:mt-40">
       <div className="flex items-start justify-between xl:items-center">
         <Image
           src={"/quirks-logo.svg"}
@@ -13,7 +13,7 @@ function Footer() {
           height={41}
           className="xl:h-[86px] xl:w-24"
         />
-        <div className="flex flex-col items-end text-right max-xl:mt-2">
+        <ul className="flex flex-col items-end text-right max-xl:mt-2">
           <Image
             src={"/quirks-text.svg"}
             alt="Quirks text logo"
@@ -21,25 +21,33 @@ function Footer() {
             height={30}
             className="xl:h-13 xl:w-40"
           />
-          <Link href={"#"} className="mt-8 opacity-30 xl:hidden">
-            Terms of Service
-          </Link>
-          <Link href={"#"} className="opacity-30 xl:hidden">
-            Privacy Policy
-          </Link>
-        </div>
+          <li>
+            <Link href={"#"} className="mt-8 opacity-30 xl:hidden">
+              Terms of Service
+            </Link>
+          </li>
+          <li>
+            <Link href={"#"} className="opacity-30 xl:hidden">
+              Privacy Policy
+            </Link>
+          </li>
+        </ul>
       </div>
       <div className="mt-[72px] flex items-start justify-between">
         <p className="text-xs opacity-30 xl:text-lg">
           © 2023 nabla benefit company
         </p>
-        <div className="gap-13 flex items-center">
-          <Link href={"#"} className="opacity-30 max-xl:hidden">
-            Terms of Service
-          </Link>
-          <Link href={"#"} className="opacity-30 max-xl:hidden">
-            Privacy Policy
-          </Link>
+        <ul className="flex items-center gap-13">
+          <li>
+            <Link href={"#"} className="opacity-30 max-xl:hidden">
+              Terms of Service
+            </Link>
+          </li>
+          <li>
+            <Link href={"#"} className="opacity-30 max-xl:hidden">
+              Privacy Policy
+            </Link>
+          </li>
           <Image
             src={"/nabla-logo.svg"}
             alt="Nabla logo"
@@ -47,9 +55,9 @@ function Footer() {
             height={30}
             className="xl:ml-2.5"
           />
-        </div>
+        </ul>
       </div>
-    </div>
+    </footer>
   );
 }
 
