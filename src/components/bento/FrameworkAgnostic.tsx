38d5eef287bdf32f8914ddda53fa39ae0eb8026c
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-16 12:19:07 +0100

    fix: :lipstick: center framework icons

--- src/components/bento/FrameworkAgnostic.tsx
+++ src/components/bento/FrameworkAgnostic.tsx
@@ -6,7 +6,7 @@ function FrameworkAgnostic() {
   return (
     <div className="bento-card-base flex flex-col gap-8 px-11 py-9 xl:col-span-3 xl:col-start-3 xl:gap-[74px] xl:px-10 xl:py-10">
       <h3 className="text-lg xl:text-bento-h">Framework Agnostic</h3>
-      <div className="flex gap-3 xl:gap-6.5">
+      <div className="flex -translate-x-4 gap-3 self-center xl:gap-6.5">
         {FRAMEWORKS.map(({ name, uri }) => (
           <Image
             key={name}
