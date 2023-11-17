3a2f0e744209f3035df5f8e4dd801c5b8665d03f
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-17 12:37:18 +0100

    refactor: :recycle: extract wallet cards

--- src/components/bento/MultiWallet.tsx
+++ src/components/bento/MultiWallet.tsx
@@ -1,16 +1,22 @@
+import { WALLET_CARDS } from "@/utils/globals";
 import Image from "next/image";
 
 function MultiWallet() {
   return (
     <div className="bento-card-base relative min-h-[433px] overflow-hidden pl-10 pt-9 xl:col-span-2 xl:col-start-4">
       <h3 className="text-lg leading-none xl:text-bento-h">Multi Wallet</h3>
-      <Image
-        src={"/wallet/wallet.png"}
-        alt="Wallets"
-        width={296}
-        height={418}
-        className="absolute -bottom-32 right-4 xl:-bottom-40 xl:right-8 xl:h-[464px] xl:w-[329px]"
-      />
+      <div className="relative -bottom-32 left-3 right-0 xl:-bottom-40 xl:left-4 xl:right-0">
+        {WALLET_CARDS.map(({ name, uri, cn }) => (
+          <Image
+            key={name}
+            alt={`${name} wallet card`}
+            src={uri}
+            width={297}
+            height={418}
+            className={"absolute " + cn}
+          />
+        ))}
+      </div>
     </div>
   );
 }
