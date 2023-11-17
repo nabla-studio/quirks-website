de49a6295c40802dceb1b84487c602b77b97750d
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-17 12:05:46 +0100

    feat: :dizzy: add rising counter animation

--- src/components/bento/TinyBundle.tsx
+++ src/components/bento/TinyBundle.tsx
@@ -1,14 +1,39 @@
+"use client";
+
+import { useCounterAnimation } from "@/utils/useCounterAnimation";
+import { motion } from "framer-motion";
+
 function TinyBundle() {
+  const { ref: kbRef, rounded: kbValue } = useCounterAnimation(0, 7);
+  const { ref: bundleSizeRef, rounded: budleSize } = useCounterAnimation(
+    0,
+    87,
+    { delay: 1 },
+  );
+
   return (
     <div className="bento-card-base flex flex-col px-10 pb-8 pt-10 xl:col-span-3 xl:col-start-3">
       <h3 className="text-lg leading-none xl:text-bento-h">Tiny Bundle</h3>
-      <div className="mt-30 flex flex-col xl:mt-14 xl:flex-row xl:items-center xl:gap-x-[60px]">
-        <div className="flex items-baseline gap-4 xl:translate-y-12">
-          <p className="text-perc-big leading-12 text-primary">7</p>
+      <div className="mt-30 flex flex-col xl:mt-14 xl:flex-row xl:items-center xl:justify-end xl:gap-x-[60px]">
+        <div className="relative flex items-baseline gap-4 xl:translate-y-12">
+          <motion.p
+            ref={kbRef}
+            className="text-perc-big leading-12 text-primary xl:absolute xl:right-28 xl:top-3"
+          >
+            {kbValue}
+          </motion.p>
           <p className="text-perc-unit">kb</p>
         </div>
         <div className="flex flex-col xl:text-center">
-          <p className="text-perc-half text-primary">87%</p>
+          <div className="flex">
+            <motion.p
+              ref={bundleSizeRef}
+              className="text-perc-half text-primary"
+            >
+              {budleSize}
+            </motion.p>
+            <span className="text-perc-half text-primary">%</span>
+          </div>
           <p className="text-1.1xl">Less than Cosmoskit *</p>
         </div>
       </div>
---
+++ src/utils/useCounterAnimation.ts
@@ -0,0 +1,27 @@
+import {
+  useMotionValue,
+  useTransform,
+  useInView,
+  animate,
+  ValueAnimationTransition,
+} from "framer-motion";
+import { useRef, useEffect } from "react";
+
+export const useCounterAnimation = (
+  from: number,
+  to: number,
+  options?: ValueAnimationTransition<number>,
+) => {
+  const count = useMotionValue(from);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const ref = useRef(null);
+  const inView = useInView(ref);
+
+  useEffect(() => {
+    if (inView) {
+      animate(count, to, { duration: 1, ease: "circOut", ...options });
+    }
+  }, [count, inView, options, to]);
+
+  return { rounded, ref };
+};
