0b69ae8e573f60e720e6f2f268fcca2a4685e61d
Davide Segullo <davidesegullo@gmail.com>
2023-11-20 22:37:25 +0100

    fix: :wrench: fix csp config

--- src/middleware.ts
+++ src/middleware.ts
@@ -33,7 +33,7 @@ export function middleware(request: NextRequest) {
     style-src 'self' ${isDev ? "'unsafe-inline'" : `'nonce-${nonce}'`};
     img-src 'self' blob: data: ${process.env.VERCEL_URL};
     font-src 'self';
-    connect-src vitals.vercel-insights.com;
+    connect-src 'self' vitals.vercel-insights.com;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
