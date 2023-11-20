71498f772d33d5ffa60a7b1aac0ccdc08a809e69
Davide Segullo <davidesegullo@gmail.com>
2023-11-20 19:15:30 +0100

    feat: :sparkles: add pause animation hover

--- src/components/bento/MultiChain.tsx
+++ src/components/bento/MultiChain.tsx
@@ -6,7 +6,7 @@ function MultiChain() {
   return (
     <div className="bento-card-base relative min-h-[334px] overflow-hidden pl-8 pt-8 xl:col-span-2 xl:col-start-1 xl:pl-10 xl:pt-10">
       <h3 className="text-lg xl:text-bento-h">Multi Chain</h3>
-      <div className="absolute -bottom-8 left-0 right-0 flex flex-col gap-2 xl:-bottom-9">
+      <div className="group absolute -bottom-8 left-0 right-0 flex flex-col gap-2 xl:-bottom-9">
         {SHOWCASE_ICONS.map((row, i) => (
           <div
             key={`row ${i}`}
@@ -14,7 +14,7 @@ function MultiChain() {
               (i + 1) % 2 === 0
                 ? "animate-marquee-sm-reverse xl:animate-marquee-reverse"
                 : "animate-marquee-sm xl:animate-marquee"
-            } xl:row-width-xl max-xl:row-width relative flex items-center gap-5 transition-transform`}
+            } group-hover:paused-animation xl:row-width-xl max-xl:row-width relative flex items-center gap-5 transition-transform`}
           >
             {row.map(({ name, uri, websiteURL }, i) => (
               <Link href={websiteURL} target="_blank" key={`${name} ${i} icon`}>
