55ee5cb119526496c17643ecb06d64e40a6f7a42
Davide Segullo <davidesegullo@gmail.com>
2023-11-21 15:36:02 +0100

    feat: :sparkles: remove animation pause

--- src/components/bento/MultiChain.tsx
+++ src/components/bento/MultiChain.tsx
@@ -6,7 +6,7 @@ function MultiChain() {
   return (
     <div className="bento-card-base relative min-h-[334px] overflow-hidden pl-8 pt-8 lg:col-span-2 lg:col-start-1 lg:pl-10 lg:pt-10">
       <h3 className="text-lg lg:text-bento-h">Multi Chain</h3>
-      <div className="group absolute -bottom-8 left-0 right-0 flex flex-col gap-2 lg:-bottom-9">
+      <div className="absolute -bottom-8 left-0 right-0 flex flex-col gap-2 lg:-bottom-9">
         {SHOWCASE_ICONS.map((row, i) => (
           <div
             key={`row ${i}`}
@@ -14,7 +14,7 @@ function MultiChain() {
               (i + 1) % 2 === 0
                 ? "animate-marquee-sm-reverse lg:animate-marquee-reverse"
                 : "animate-marquee-sm lg:animate-marquee"
-            } group-hover:paused-animation lg:row-width-xl max-lg:row-width relative flex items-center gap-5 transition-transform`}
+            } lg:row-width-xl max-lg:row-width relative flex items-center gap-5 transition-transform`}
           >
             {row.map(({ name, uri, websiteURL }, i) => (
               <Link href={websiteURL} target="_blank" key={`${name} ${i} icon`}>
