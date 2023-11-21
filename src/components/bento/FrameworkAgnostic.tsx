bdba8c43fec29c31b1405fd32f4988cfa99c622c
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-21 13:06:25 +0100

    fix: :lipstick: update framework icons positioning

--- src/components/bento/FrameworkAgnostic.tsx
+++ src/components/bento/FrameworkAgnostic.tsx
@@ -5,7 +5,7 @@ function FrameworkAgnostic() {
   return (
     <div className="bento-card-base flex flex-col gap-8 px-11 py-9 lg:col-span-3 lg:col-start-3 lg:gap-18.5 lg:px-10 lg:py-10">
       <h3 className="text-lg lg:text-bento-h">Framework Agnostic</h3>
-      <div className="flex gap-3 self-center lg:-translate-x-4 lg:gap-6.5">
+      <div className="grid w-full grid-cols-3 justify-items-center gap-3 self-center lg:gap-6.5">
         {FRAMEWORKS.map(({ name, uri }) => (
           <Image
             key={name}
@@ -13,7 +13,7 @@ function FrameworkAgnostic() {
             alt={`${name} icon`}
             width={77}
             height={77}
-            className="lg:h-framework-icon lg:w-framework-icon"
+            className="h-auto w-full max-w-[180px]"
           />
         ))}
       </div>
