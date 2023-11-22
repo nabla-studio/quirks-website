e3a6d05dcd43f397edf21a37a2e4bbb8eb04a789
Davide Segullo <davidesegullo@gmail.com>
2023-11-22 15:51:35 +0100

    chore: :wrench: add script https

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <Script
-        src={process.env.VERCEL_URL}
+        src={`https://${process.env.VERCEL_URL}`}
         strategy="afterInteractive"
         nonce={nonce ?? undefined}
       />
