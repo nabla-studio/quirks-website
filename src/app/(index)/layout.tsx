a4b4cb0d2e1163cf6114dbfb7a83fcf0e3debfec
Davide Segullo <davidesegullo@gmail.com>
2024-02-13 23:57:46 +0100

    fix: :bug: fix metadata base

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,5 +1,36 @@
 import { Provider } from "./providers";
 import "./globals.css";
+import type { Metadata } from "next";
+
+const baseUrl = process.env.SITE_URL
+  ? new URL(`https://${process.env.SITE_URL}`)
+  : new URL("https://www.quirks.nabla.studio");
+
+export const metadata: Metadata = {
+  metadataBase: baseUrl,
+  title: "Quirks",
+  description:
+    "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+  openGraph: {
+    locale: "en_US",
+    type: "website",
+    title: "Quirks",
+    description:
+      "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+    url: baseUrl,
+    siteName: "Quirks",
+  },
+  twitter: {
+    title: "Quirks",
+    site: "@nabla_hq",
+    description:
+      "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+    card: "summary_large_image",
+  },
+  alternates: {
+    canonical: "/",
+  },
+};
 
 export default function RootLayout({
   children,
--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -50,10 +50,36 @@ const Logo = (props: SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
+const baseUrl = process.env.SITE_URL
+  ? new URL(`https://${process.env.SITE_URL}`)
+  : new URL("https://www.quirks.nabla.studio");
+
 export const metadata: Metadata = {
+  metadataBase: baseUrl,
   title: {
     template: "%s | Quirks",
     default: "Quirks",
+  },
+  description:
+    "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+  openGraph: {
+    locale: "en_US",
+    type: "website",
+    title: "Quirks",
+    description:
+      "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+    url: baseUrl,
+    siteName: "Quirks",
+  },
+  twitter: {
+    title: "Quirks",
+    site: "@nabla_hq",
+    description:
+      "Quirks is a universal wallet adapter that easily connect your dapp with existing wallets in the Cosmos blockchain ecosystem.",
+    card: "summary_large_image",
+  },
+  alternates: {
+    canonical: "/",
   },
 };
 
