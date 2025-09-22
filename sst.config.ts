5fe33ad9a5efc340816ff196c5bb6502e2975fbb
Giorgio Nocera <giorgio.nocera@nabla.studio>
2025-09-22 18:17:42 +0200

    chore: :wrench: add domain configuration
    Adding the domain configuration for sst website deployment.

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
 
+// Extract environment variables
+const { DOMAIN_NAME, DOMAIN_CERT_ARN } = process.env;
+
 export default $config({
   app(input) {
     return {
@@ -11,6 +14,17 @@ export default $config({
     };
   },
   async run() {
-    new sst.aws.Nextjs("Website");
+    new sst.aws.Nextjs("Website", {
+      // Add custom domain only if provided
+      ...(DOMAIN_NAME &&
+        DOMAIN_CERT_ARN && {
+        domain: {
+          name: DOMAIN_NAME,
+          dns: false,
+          cert: DOMAIN_CERT_ARN,
+          redirects: [`www.${DOMAIN_NAME}`],
+        },
+      }),
+    });
   },
 });
